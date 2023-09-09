5a42a02c676747c682e64b59c033b737481d582f
Roi Tanimoto <roi@hawaii.edu>
2023-09-08 15:49:36 -1000

    Finish Band Name Generator Exercise

---
+++ Band-Name-Generator/main.py
@@ -0,0 +1,15 @@
+"""
+Band Name Generator
+Designed to practice the Python Basics
+1. Print method
+2. Variables
+3. Input
+4. F-strings
+5. Comments
+
+"""
+
+print("Welcome to the Band Name Generator.")
+userCity = input("What's the name of the city you grew up in?\n")
+userPet = input("What's your pet's name?\n")
+print(f"Your band name could be {userCity} {userPet}")
